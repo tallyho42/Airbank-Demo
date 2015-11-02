993fd5f50bddde61df568f8279bbac450ab281f0
Premysl Semerad <premysl.semerad@gmail.com>
2015-11-02 10:49:56 +0100

    created all the future files, grouped project files accordingly

---
+++ airbankDemoSwift/airbankDemoSwift/Controllers/Transactions/ABTransactionsDetailVC.swift
@@ -0,0 +1,9 @@
+//
+//  ABTransactionsDetailVC.swift
+//  airbankDemoSwift
+//
+//  Created by Premysl Semerad on 02/11/15.
+//  Copyright © 2015 Premysl Semerad. All rights reserved.
+//
+
+import Foundation
---
+++ airbankDemoSwift/airbankDemoSwift/Views/ABMasterCell.swift
@@ -0,0 +1,9 @@
+//
+//  ABMasterCell.swift
+//  airbankDemoSwift
+//
+//  Created by Premysl Semerad on 02/11/15.
+//  Copyright © 2015 Premysl Semerad. All rights reserved.
+//
+
+import Foundation
